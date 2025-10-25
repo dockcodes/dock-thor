ebf42dbb80b2ef3468a4d978172e3def67719dba
Jacek <j.labudda@dock.codes>
2025-10-25 19:15:58 +0200

    improved

--- dock_thor/serializer.py
+++ dock_thor/serializer.py
@@ -1,19 +1,64 @@
 import json
+import platform
+from datetime import datetime, timezone
 from .models import Event
 
 class PayloadSerializer:
-    @staticmethod
-    def serialize(event: Event) -> str:
-        """Zamienia Event w JSON zgodny z API."""
-        return json.dumps({
+
+    def serialize(event: Event, transaction: str | None = None, request: dict | None = None, user: dict | None = None,
+                  http_status_code: int | None = None) -> str:
+        now_iso = datetime.now(timezone.utc)
+
+        payload = {
             "event_id": event.event_id,
             "timestamp": event.timestamp,
-            "level": event.level,
+            "sdk": {
+                "name": "pab-client",
+                "version": "0.9.4"
+            },
+            "environment": event.environment,
             "platform": event.platform,
             "server_name": event.server_name,
-            "environment": event.environment,
             "message": event.message,
             "extra": event.extra,
             "tags": event.tags,
-            "exception": event.exception,
-        }, ensure_ascii=False)
+            "user": user or {},
+            "last_issued_at": now_iso,
+            "status": "Pending",
+            "contexts": {
+                "os": {
+                    "name": platform.system(),
+                    "version": platform.version(),
+                    "build": platform.release(),
+                    "kernel_version": platform.platform()
+                },
+                "runtime": [platform.python_implementation(), platform.python_version()]
+            }
+        }
+
+        if event.exception:
+            payload["exception"] = {
+                "values": [{
+                    "type": event.exception.get("type"),
+                    "value": event.message,
+                    "stacktrace": event.exception.get("traceback", "")
+                }]
+            }
+
+        if request:
+            payload["request"] = {
+                "url": request.get("url", ""),
+                "method": request.get("method", ""),
+                "headers": request.get("headers", {})
+            }
+
+        if transaction:
+            payload["transaction"] = transaction
+            payload["tags"]["http.status_code"] = http_status_code
+            payload["sent_at"] = now_iso
+            payload.setdefault("contexts", {}).setdefault("trace", {})["data"] = {
+                "url": request.get("url") if request else "",
+                "method": request.get("method") if request else ""
+            }
+
+        return json.dumps(payload, ensure_ascii=False)
